1af752d14253b3ce8a8f75b2c90013b19c691503
Abass14 <abasstoy14@gmail.com>
2021-12-21 23:40:50 +0100

    splice

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-//the slice method returns a shallow copy of a portion of an array, similar to substring
-//does not modify the original array
+//splice methode removes or replace an existing element from an array
+//has startingIndex, Count, and elements to add
+//modifies existing array 
 
 //ex1
-//returning array from index 3 to 6
-//does not include the last index
+const numbers = [1,2,3,4,5,6]
 
-const numbers = [1,2,3,4,5,6,7,8,9]
+const deleted = numbers.splice(3, 3, 7, 8) //saving in a variable returns the deleted items
 
-const result = numbers.slice(3, 6)
-
-//to get the last 3 items
-const lastThree = numbers.slice(-3)
-
-//to get all items starting from a specified index
-const startFromIndex2 = numbers.slice(2)
-
-console.log(startFromIndex2)
-console.log(lastThree)
-console.log(result)
+console.log(numbers) //returns items left in array
+console.log(deleted) //returns deleted items in the array

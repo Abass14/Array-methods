f4164ca3744dba5a790f6479ea04d30345bf99db
Abass14 <abasstoy14@gmail.com>
2021-12-21 23:53:46 +0100

    sort

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-//splice methode removes or replace an existing element from an array
-//has startingIndex, Count, and elements to add
-//modifies existing array 
+//sort method sorts the array by manipulating the array
 
 //ex1
-const numbers = [1,2,3,4,5,6]
+const names = ['Rukayat', 'Abass', 'Mustapha', 'Richard', 'Martins', 'Joyce']
 
-const deleted = numbers.splice(3, 3, 7, 8) //saving in a variable returns the deleted items
+names.sort()
 
-console.log(numbers) //returns items left in array
-console.log(deleted) //returns deleted items in the array
+console.log(names)
+
+//ex2
+//to use sort on numbers you need to add a compareFunction callback
+const numbers = [82, 35, 269, 267, 5, 62, 83, 9]
+
+//compare function => (a, b) => a - b
+numbers.sort((a, b) => a - b)
+
+console.log(numbers)
+
+//ex3
+const products = [
+    {
+        name: 'Dell',
+        price: 2500
+    },
+    {
+        name: 'Hp',
+        price: 3500
+    },
+    {
+        name: 'Apple',
+        price: 1000
+    },
+]
+
+products.sort((a, b) => {
+    return a.price - b.price
+})
+
+console.log(products)

2dd57b9e432e161588c64523d62074890dcbea0f
Abass14 <abasstoy14@gmail.com>
2021-12-21 15:17:54 +0100

    map

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
-//for each method
-//for each method takes a callback function
-// the call back takse in the arrayItem, index and arr itself
+//the map method executes a given function on every element in an array and returns a new array
+//it does not return a modified existing array, rather a new array gotten from a modified existing array
 
-//1
-const arr = [1,2,3,4,5]
+//ex1
+const numbers = [1,2,3,4,5]
 
-//summing all items
-let sum = 0
-
-arr.forEach((arrayItem, index, arr) => {
-    // console.log(arrayItem)
-    // console.log('arr'+index+ ' = '+ arrayItem)
-    // console.log(arr)
-    sum += arrayItem
+const numbersDouble = numbers.map((value, index, arr) => {
+    return value * 2
 })
 
-console.log(sum)
+const multiplyIndex = numbers.map((value, index) => {
+    return value * index
+})
 
-//2
-//to check how many times a character appear in an array
-const characters = ['a', 'b', 'a', 'd', 'a', 'f', 'f']
 
-const count = {}
-let counter = 0
+console.log(numbersDouble)
+console.log(multiplyIndex)
+console.log(numbers)
 
-characters.forEach(items => {
-    // if (count[items]) {
-    //     count[items]++
-    // }else{
-    //     count[items] = 1
-    // }
 
-    if (items == 'f') {
-        counter++
+//ex2
+
+const products = [
+    {
+        name: 'dell',
+        price: 1000,
+        count: 5
+    }, 
+    {
+        name: 'hp',
+        price: 2000,
+        count: 3
+    }, 
+    {
+        name: 'apple',
+        price: 7500,
+        count: 2
+    }, 
+]
+
+const totalProductValues = products.map((items) => {
+    // return items.price * items.count
+    return {
+        name: items.name,
+        value: items.price * items.count
     }
 })
 
-console.log(counter)
+console.log(totalProductValues)
 
 
+//ex3
+const str = ['1', '2', '3', '4', '5']
+
+const num = str.map(Number)
+
+console.log(str)

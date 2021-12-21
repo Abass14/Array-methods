359defb877b73f9220cb6de1bf1b7baa61f9e40d
Abass14 <abasstoy14@gmail.com>
2021-12-21 23:29:00 +0100

    slice

--- index.js
+++ index.js
@@ -1,54 +1,20 @@
-//the reduce method executes a callback method on all the items of an array and returns one/single output value.
-//it takses in a callback function and initial value
-//the callback can take 4 params: the accumulator, value, index and array
-//the accumulator params keeps track of the value that'll be returned at the end
+//the slice method returns a shallow copy of a portion of an array, similar to substring
+//does not modify the original array
 
 //ex1
+//returning array from index 3 to 6
+//does not include the last index
 
-const numbers = [1,2,3,4,5]
+const numbers = [1,2,3,4,5,6,7,8,9]
 
-const total = numbers.reduce((accumulator, value, index, arr) => {
-    return accumulator + value
-}, 0)
+const result = numbers.slice(3, 6)
 
-console.log(total)
+//to get the last 3 items
+const lastThree = numbers.slice(-3)
 
-//ex2
-//returning max val in an array
-const listNum = [1,2,3,4,5,6,7]
+//to get all items starting from a specified index
+const startFromIndex2 = numbers.slice(2)
 
-const maxValue = listNum.reduce((accumulator, value) => {
-    if (accumulator > value) {
-        return accumulator
-    }else{
-        return value
-    }
-}, -Infinity)
-
-console.log(maxValue)
-
-//ex3
-
-const store = [
-    {
-        product: 'Toshiba',
-        value: 1000,
-        count: 15
-    },
-    {
-        product: 'Hp',
-        value: 3000,
-        count: 10
-    },
-    {
-        product: 'Apple',
-        value: 15000,
-        count: 5
-    },
-]
-
-const totalStoreValue = store.reduce((acc, item) => {
-    return acc + (item.value * item.count)
-}, 0)
-
-console.log(totalStoreValue)
+console.log(startFromIndex2)
+console.log(lastThree)
+console.log(result)

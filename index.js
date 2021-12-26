bb4144fc2a75d894a83e5d5a13e9bbcc7cdd1df1
Abass14 <abasstoy14@gmail.com>
2021-12-27 00:20:16 +0100

    pop

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-//the reverse method is used to reverse an array
-//the push method allows to add one or more element at the end of the array
+//the pop method removed the last item in an array and return the array
 
-//ex1
+// ex1
+
 const numbers = [1,2,3,4]
 
-numbers.push(5)
+numbers.pop()
 
 console.log(numbers)
+
+//ex2
+
+// when stored in a variable, it returns the value removed
+
+const nums = [1,2,3,4,5]
+
+const lastItem = nums.pop()
+
+console.log(lastItem)
+console.log(nums)

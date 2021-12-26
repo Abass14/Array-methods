36b8087aa364ffba87a4f83f81b4b48cad1e26c7
Abass14 <abasstoy14@gmail.com>
2021-12-26 23:44:09 +0100

    include

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-//fill method changes all the items in an array with a provided value
-//modifies the existing array
+//the include methods makes it possible to know if an element is included in an array
 
 //ex1
-const numbers = [1,2,3,4,5]
+const names = ['Abass', 'Adisa', 'Richard']
 
-numbers.fill(0)
+const result = names.includes('Abass')
 
-console.log(numbers)
-
-//start and end index can be specified => value, startIndex, endIndex
-const nums = [1,2,3,4,5,6]
-
-nums.fill(0, 1, 3)
-
-console.log(nums)
-
-//ex3
-
-const fillInNumbers = (n) => {
-    return Array(n).fill(0).map((_, index) => index + 1)
-}
-
-console.log(fillInNumbers(10))
+console.log(result)

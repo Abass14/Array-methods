b4d335a7df262d88f0b4f49f139d7382668712da
Abass14 <abasstoy14@gmail.com>
2021-12-27 00:15:41 +0100

    push

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 //the reverse method is used to reverse an array
-//it modifies the existing array
+//the push method allows to add one or more element at the end of the array
 
-const numbers = [1,2,3,4,5]
+//ex1
+const numbers = [1,2,3,4]
 
-numbers.reverse()
+numbers.push(5)
 
 console.log(numbers)
-
-//to uses reverse without modifying the existing array
-
-const nums = [1,2,3,4,5]
-
-const reverseCopy = nums.concat().reverse() 
-//OR
-const revCody = [...nums].reverse()
-
-console.log(nums)
-console.log(reverseCopy)
-console.log(revCody)
-
-//ex3
-//reverse characters of a string
-const str = 'coding is fun'
-
-const newStr = str.split('').reverse().join('')
-
-console.log(newStr)

af72748aa3ce6dd4d0d23b4c421e2e66cbecb1d7
Abass14 <abasstoy14@gmail.com>
2021-12-27 00:10:26 +0100

    reverse

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ const numbers = [1,2,3,4,5]
 numbers.reverse()
 
 console.log(numbers)
+
+//to uses reverse without modifying the existing array
+
+const nums = [1,2,3,4,5]
+
+const reverseCopy = nums.concat().reverse() 
+//OR
+const revCody = [...nums].reverse()
+
+console.log(nums)
+console.log(reverseCopy)
+console.log(revCody)
+
+//ex3
+//reverse characters of a string
+const str = 'coding is fun'
+
+const newStr = str.split('').reverse().join('')
+
+console.log(newStr)
